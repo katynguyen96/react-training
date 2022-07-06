f9c485e01facec1033d7dae47bee878e33c1aba4
katynguyen96 <56082798+katynguyen96@users.noreply.github.com>
2022-07-06 17:29:12 +0700

    practice with styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from 'react'
-import Setup from './example/9-custom-hooks/setup/2-useFetch'
+import styled from 'styled-components'
 
 function App() {
-  return (
-  	<div className='container'>
-  		<Setup/>
-  	</div>
+  const Title = styled.h1`
+  	font-size: 1.5em;
+  	text-align: center;
+  	color: palevioletred
+  `;
+  const Wrapper = styled.section`
+	padding: 4em;
+	background: papayawhip;
+  `;
+
+  const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.style ? "palevioletred" : "white"};
+  color: ${props => props.style ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+const ReversedButton = props => <Button {...props} children={props.children.split('').reverse()} />
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: ${props => props.inputColor || "palevioletred"};
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+return (
+  <div>
+    <Button as="a" href="#">Normal</Button>
+    <Button as={ReversedButton}>Custom Button with Normal Button styles</Button>
+    <Button style>Primary</Button>
+    <TomatoButton>Tomato Button</TomatoButton>
+    <Input defaultValue="@probablyup" type="text" />
+    <Input defaultValue="@geelen" type="text" inputColor="rebeccapurple" />
+  </div>
   )
 }
 
